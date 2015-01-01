361f6487adb310ead1c339dc34bdf4a28b798b3e
dak180 <dak180@users.sourceforge.net>
2014-12-31 23:05:51 -0500

    Track 4s for weirdness level.

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main() {
 	int iRollNum = 0; // The result of a dice roll
 	int iSuccesses = 0; // The number of Successes rolled
 	int iTens = 0; // The number of 10s rolled
+	int iFours = 0; // The number of 4s rolled
 	int iAutoSucc = 0; // The number of automatic successes
 	srand( (unsigned int) time(NULL) ); // Get the current time to seed random number generation
 
@@ -26,6 +27,7 @@ int main() {
 		// Roll the dice
 		iRollNum = (rand() % 10) + 1;
 
+		// Check the results of the roll
 		if ( iRollNum >= 7 ) {
 
 			iSuccesses++;
@@ -37,6 +39,10 @@ int main() {
 				iTens++;
 			}
 		}
+		if ( iRollNum == 4 ) {
+			// Check for 4s
+			iFours++;
+		}
 
 		printf("\nRolled a %d.",iRollNum);
 
@@ -45,7 +51,7 @@ int main() {
 
 	iSuccesses += iAutoSucc;
 
-	printf("\n\nYou got %d Success(es), with %d 10(s) and %d auto(s).\n",iSuccesses,iTens,iAutoSucc);
+	printf("\n\nYou got %d Success(es), with %d 10(s), %d auto(s) and %d 4(s).\n",iSuccesses,iTens,iAutoSucc,iFours);
 
     return 0;
 }
